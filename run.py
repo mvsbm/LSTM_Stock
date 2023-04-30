da5efe45aceb4c430b14056d735d9eefa36e6650
mvsbm <67880879+mvsbm@users.noreply.github.com>
2023-04-30 12:53:14 +0900

    minor fix

--- run.py
+++ run.py
@@ -1,190 +1,135 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras import layers
-import os
-from tensorflow.keras.models import load_model
-from copy import deepcopy
-from tqdm import tqdm
+import datetime as dt
+from datetime import datetime
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint, TensorBoard
+from sklearn.preprocessing import StandardScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Dropout
+from keras.optimizers import Adam
+from pylab import rcParams
 
-MODEL_SAVE_PATH = 'saved_model.h5'
-def load_data(stock_csv, interest_rate_csv):
-    df = pd.read_csv(stock_csv)
-    interest_rate_df = pd.read_csv(interest_rate_csv)
+def datetime_to_timestamp(x):
+    return datetime.strptime(x.strftime('%Y%m%d'), '%Y%m%d')
 
-    df['Date'] = pd.to_datetime(df['Date'])
-    interest_rate_df['DATE'] = pd.to_datetime(interest_rate_df['DATE'])
+def load_data():
+    stock_dataset_train = pd.read_csv('STAG.csv')
+    interest_rate_dataset = pd.read_csv('US10Y.csv')
 
-    df = df.rename(columns={'Date': 'DATE'})
-    df = df.merge(interest_rate_df, on='DATE', how='left')
+    stock_dataset_train['DGS10'] = interest_rate_dataset['DGS10']
+    cols = list(stock_dataset_train)[1:7]
 
-    df = df.fillna(method='ffill')
+    datelist_train = list(stock_dataset_train['Date'])
+    datelist_train = [dt.datetime.strptime(date, '%Y-%m-%d').date() for date in datelist_train]
 
-    df = df.set_index('DATE')
-    return df
+    return stock_dataset_train, cols, datelist_train
 
-def df_to_windowed_df(dataframe, first_date_str, last_date_str, n=10):
-    first_date = pd.to_datetime(first_date_str)
-    last_date = pd.to_datetime(last_date_str)
+def preprocess_data(stock_dataset_train, cols):
+    stock_dataset_train = stock_dataset_train[cols].astype(str)
+    for i in cols:
+        for j in range(0, len(stock_dataset_train)):
+            stock_dataset_train[i][j] = stock_dataset_train[i][j].replace(',', '')
 
-    target_date = first_date
+    stock_dataset_train = stock_dataset_train.astype(float)
 
-    dates = []
-    X, Y = [], []
+    return stock_dataset_train
 
-    while target_date <= last_date:
-        df_subset = dataframe.loc[:target_date].tail(n + 1)
+def create_training_set(stock_dataset_train):
+    training_set = stock_dataset_train.as_matrix()
 
-        if len(df_subset) != n + 1:
-            print(f'Error: Window of size {n} is too large for date {target_date}')
-            return
+    sc = StandardScaler()
+    training_set_scaled = sc.fit_transform(training_set)
 
-        values = df_subset['Close'].to_numpy()
-        interest_rate_values = df_subset['DGS10'].to_numpy()[:-1]
-        x, y = np.column_stack((values[:-1], interest_rate_values)), values[-1]
+    sc_predict = StandardScaler()
+    sc_predict.fit_transform(training_set[:, 0:1])
 
-        dates.append(target_date)
-        X.append(x)
-        Y.append(y)
+    return training_set, training_set_scaled, sc, sc_predict
 
-        target_date += pd.Timedelta(days=1)
+def prepare_data_structure(training_set_scaled, stock_dataset_train):
+    X_train = []
+    y_train = []
 
-    ret_df = pd.DataFrame({})
-    ret_df['Target Date'] = dates
+    n_future = 60
+    n_past = 90
 
-    X = np.array(X)
-    for i in range(0, n):
-        ret_df[f'Target-{n - i} Close'] = X[:, i, 0]
-        ret_df[f'Target-{n - i} Interest Rate'] = X[:, i, 1]
+    for i in range(n_past, len(training_set_scaled) - n_future + 1):
+        X_train.append(training_set_scaled[i - n_past:i, 0:stock_dataset_train.shape[1] - 1])
+        y_train.append(training_set_scaled[i + n_future - 1:i + n_future, 0])
 
-    ret_df['Target'] = Y
+    X_train, y_train = np.array(X_train), np.array(y_train)
 
-    return ret_df
+    return X_train, y_train, n_future, n_past
 
-def windowed_df_to_date_X_y(windowed_dataframe):
-    df_as_np = windowed_dataframe.to_numpy()
+def create_model(stock_dataset_train, n_past):
+    model = Sequential()
 
-    dates = df_as_np[:, 0]
+    model.add(LSTM(units=64, return_sequences=True, input_shape=(n_past, stock_dataset_train.shape[1] - 1)))
+    model.add(LSTM(units=10, return_sequences=False))
+    model.add(Dropout(0.25))
+    model.add(Dense(units=1, activation='linear'))
 
-    middle_matrix = df_as_np[:, 1:-1]
-    X = middle_matrix.reshape((len(dates), middle_matrix.shape[1], 1))
-
-    Y = df_as_np[:, -1]
-
-    return dates, X.astype(np.float32), Y.astype(np.float32)
-
-def train_model(X_train, y_train, X_val, y_val):
-    model = Sequential([layers.Input((3, 2)),
-                        layers.LSTM(64),
-                        layers.Dense(32, activation='relu'),
-                        layers.Dense(32, activation='relu'),
-                        layers.Dense(1)])
-
-    model.compile(loss='mse',
-                  optimizer=Adam(learning_rate=0.001),
-                  metrics=['mean_absolute_error'])
-
-    model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=100)
+    model.compile(optimizer=Adam(learning_rate=0.01), loss='mean_squared_error')
 
     return model
 
-def plot_results(dates, y_true, y_pred, labels, title):
-    for d, y_t, y_p, label in zip(dates, y_true, y_pred, labels):
-        plt.plot(d, y_t)
-        plt.plot(d, y_p)
-        plt.legend(label)
+def train_model(model, X_train, y_train):
+    es = EarlyStopping(monitor='val_loss', min_delta=1e-10, patience=10, verbose=1)
+    rlr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=10, verbose=1)
+    mcp = ModelCheckpoint(filepath='weights.h5', monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=True)
+    tb = TensorBoard('logs')
 
-    plt.title(title)
+    history = model.fit(X_train, y_train, shuffle=True, epochs=30, callbacks=[es, rlr, mcp, tb], validation_split=0.2, verbose=1, batch_size=256)
+
+    return history
+
+def predict_stock_prices(model, X_train, y_train, n_future, n_past, sc_predict):
+    datelist_future = pd.date_range(datelist_train[-1], periods=n_future, freq='1d').tolist()
+    datelist_future_ = [this_timestamp.date() for this_timestamp in datelist_future]
+
+    predictions_future = model.predict(X_train[-n_future:])
+    predictions_train = model.predict(X_train[n_past:])
+
+    y_pred_future = sc_predict.inverse_transform(predictions_future)
+    y_pred_train = sc_predict.inverse_transform(predictions_train)
+
+    return y_pred_future, y_pred_train, datelist_future
+
+def plot_predictions(y_pred_future, y_pred_train, datelist_train, datelist_future, dataset_train):
+    PREDICTIONS_FUTURE = pd.DataFrame(y_pred_future, columns=['Open']).set_index(pd.Series(datelist_future))
+    PREDICTION_TRAIN = pd.DataFrame(y_pred_train, columns=['Open']).set_index(pd.Series(datelist_train[2 * n_past + n_future -1:]))
+
+    PREDICTION_TRAIN.index = PREDICTION_TRAIN.index.to_series().apply(datetime_to_timestamp)
+
+    rcParams['figure.figsize'] = 14, 5
+
+    START_DATE_FOR_PLOTTING = '2012-06-01'
+
+    plt.plot(PREDICTIONS_FUTURE.index, PREDICTIONS_FUTURE['Open'], color='r', label='Predicted Stock Price')
+    plt.plot(PREDICTION_TRAIN.loc[START_DATE_FOR_PLOTTING:].index, PREDICTION_TRAIN.loc[START_DATE_FOR_PLOTTING:]['Open'], color='orange', label='Training predictions')
+    plt.plot(dataset_train.loc[START_DATE_FOR_PLOTTING:].index, dataset_train.loc[START_DATE_FOR_PLOTTING:]['Open'], color='b', label='Actual Stock Price')
+
+    plt.axvline(x = min(PREDICTIONS_FUTURE.index), color='green', linewidth=2, linestyle='--')
+
+    plt.grid(which='major', color='#cccccc', alpha=0.5)
+
+    plt.legend(shadow=True)
+    plt.title('Predictions and Actual Stock Prices', family='Arial', fontsize=12)
+    plt.xlabel('Timeline', family='Arial', fontsize=10)
+    plt.ylabel('Stock Price Value', family='Arial', fontsize=10)
+    plt.xticks(rotation=45, fontsize=8)
     plt.show()
 
-
-import os
-from tensorflow.keras.models import load_model
-
-MODEL_SAVE_PATH = "lstm_stock_model.h5"
-
-
-
-MODEL_SAVE_PATH = "lstm_stock_model.h5"
-
-def split_data(dates, X, y):
-    q_80 = int(len(dates) * .8)
-    q_90 = int(len(dates) * .9)
-
-    dates_train, X_train, y_train = dates[:q_80], X[:q_80], y[:q_80]
-    dates_val, X_val, y_val = dates[q_80:q_90], X[q_80:q_90], y[q_80:q_90]
-    dates_test, X_test, y_test = dates[q_90:], X[q_90:], y[q_90:]
-
-    train_data = (dates_train, X_train, y_train)
-    val_data = (dates_val, X_val, y_val)
-    test_data = (dates_test, X_test, y_test)
-
-    return train_data, val_data, test_data
-
-
-def build_model():
-    model = Sequential([
-        layers.Input((3, 2)),
-        layers.LSTM(64),
-        layers.Dense(32, activation='relu'),
-        layers.Dense(32, activation='relu'),
-        layers.Dense(1)
-    ])
-
-    model.compile(loss='mse',
-                  optimizer=Adam(learning_rate=0.001),
-                  metrics=['mean_absolute_error'])
-
-    return model
-
-
 def main():
-    # Load stock data and interest rate data
-    df = load_data('STAG.csv', 'US10Y.csv')
+    stock_dataset_train, cols, datelist_train = load_data()
+    stock_dataset_train = preprocess_data(stock_dataset_train, cols)
+    training_set, training_set_scaled, sc, sc_predict = create_training_set(stock_dataset_train)
+    X_train, y_train, n_future, n_past = prepare_data_structure(training_set_scaled, stock_dataset_train)
+    model = create_model(stock_dataset_train, n_past)
+    history = train_model(model, X_train, y_train)
+    y_pred_future, y_pred_train, datelist_future = predict_stock_prices(model, X_train, y_train, n_future, n_past, sc_predict)
+    plot_predictions(y_pred_future, y_pred_train, datelist_train, datelist_future, stock_dataset_train)
 
-    # Create a windowed DataFrame
-    windowed_df = df_to_windowed_df(df, '2021-03-25', '2022-03-23', n=6)
-
-    # Convert windowed DataFrame to date, X, and y arrays
-    dates, X, y = windowed_df_to_date_X_y(windowed_df)
-    q_80 = int(len(dates) * .8)
-    q_90 = int(len(dates) * .9)
-
-    dates_train, X_train, y_train = dates[:q_80], X[:q_80], y[:q_80]
-    dates_val, X_val, y_val = dates[q_80:q_90], X[q_80:q_90], y[q_80:q_90]
-    dates_test, X_test, y_test = dates[q_90:], X[q_90:], y[q_90:]
-
-    # Split the data into train, validation, and test sets
-    train_data, val_data, test_data = split_data(dates, X, y)
-
-    # Load the saved model or train and save the model
-    if os.path.exists(MODEL_SAVE_PATH):
-        model = load_model(MODEL_SAVE_PATH)
-        print("Model loaded from", MODEL_SAVE_PATH)
-    else:
-        model = build_model()
-        model = train_model(X_train, y_train, X_val, y_val)
-        model.save(MODEL_SAVE_PATH)
-        print("Model saved to", MODEL_SAVE_PATH)
-
-    # Evaluate the model on the train, validation, and test sets
-    plot_evaluated_model(model, train_data, val_data, test_data)
-
-    # Predict future 30 days from April 30, 2023
-    recursive_dates, recursive_predictions = predict_recursive(model, X, 30)
-
-    # Plot the predictions
-    plot_recursive_predictions(recursive_dates, recursive_predictions)
-
-    # Plot all the data
-    plot_all(model, train_data, val_data, test_data, recursive_dates, recursive_predictions)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
-
-
 
